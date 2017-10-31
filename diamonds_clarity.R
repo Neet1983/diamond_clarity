88fb1e7e7cbb90f0d1ee738f936c82db1db656f0
Puneet Satyawan <puneetsatyawan@gmail.com>
2017-10-31 10:58:44 -0400

    Add files via upload

--- diamonds_clarity.R
+++ diamonds_clarity.R
@@ -1,13 +1,18 @@
+#using ggplot to plot a smooth scatterplot for diamond carat against price
+
+#remove objects from the workspace
 rm(list = ls())
 
-
+#read a csv file into the workspace
 mydata <- read.csv(file.choose())
 
+#install the ggplot 2 package
 install.packages("ggplot2")
 
+#activate the gglot 2 package
 library("ggplot2")
 
-
+#plot the graph
 ggplot(data = mydata[mydata$carat<2.5, ], 
        aes(x=carat, y=price, color = clarity)) + 
   geom_point(alpha = 0.1) +
